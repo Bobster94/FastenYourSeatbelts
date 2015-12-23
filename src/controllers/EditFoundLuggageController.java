29a8f9a77de784b46a470404e9bd42f183fcf763
Bas Broerse <basbroerse@hotmail.com>
2015-12-23 21:20:41 +0100

    edit found luggage

--- src/controllers/EditFoundLuggageController.java
+++ src/controllers/EditFoundLuggageController.java
@@ -48,19 +48,19 @@ public class EditFoundLuggageController implements Initializable {
         }
         return screen;
     }
-    
+    private static int id;
     @FXML private TextField txtExtra;
     @FXML private TextField txtBarcode;
     @FXML private TextField txtLostAtAirport;
     @FXML private TextField txtFoundAtAirport;
     @FXML private TextField txtFlightNumber;
     @FXML private TextField txtDate;
-    @FXML private ComboBox  txtMaterial;
-    @FXML private ComboBox  txtColor;
-    @FXML private ComboBox  txtType;
-    @FXML private ComboBox  txtBrand;
-    @FXML private ComboBox  txtWeight;
-    @FXML private ComboBox  txtSize;
+    @FXML private ComboBox  cbMaterial;
+    @FXML private ComboBox  cbColor;
+    @FXML private ComboBox  cbType;
+    @FXML private ComboBox  cbBrand;
+    @FXML private ComboBox  cbWeight;
+    @FXML private ComboBox  cbSize;
     
     /*
     *
@@ -75,17 +75,20 @@ public class EditFoundLuggageController implements Initializable {
         String lostAirport = txtLostAtAirport.getText();
         String flightNumber = txtFlightNumber.getText();
         String date = txtDate.getText();
-        String material = txtMaterial.getValue().toString();
-        String color = txtColor.getValue().toString();
-        String type = txtType.getValue().toString();
-        String brand = txtBrand.getValue().toString();
-        String weight = txtWeight.getValue().toString();
-        String size = txtSize.getValue().toString();
+        String material = cbMaterial.getValue().toString();
+        String color = cbColor.getValue().toString();
+        String type = cbType.getValue().toString();
+        String brand = cbBrand.getValue().toString();
+        String weight = cbWeight.getValue().toString();
+        String size = cbSize.getValue().toString();
         
         try(Connection conn = Database.initDatabase()) {
-            String SQL = "INSERT INTO luggage (brand,color,type,weight,size,barcode,lostAirport,"
-                    + "foundAirport,extra,lostFound,material,date,flightNumber,idEmployee) "
-                    + "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+            String SQL = "UPDATE luggage "
+                    + "SET brand = ?, color = ?, type = ?, weight = ?,"
+                    + " size = ?, barcode = ?, lostAirport = ?, foundAirport = ?,"
+                    + " extra = ?, lostFound = ?, material = ?, date = ?,"
+                    + " flightNumber = ?, idEmployee = ? "
+                    + "WHERE id = ?";
            
             PreparedStatement preparedStatement = conn.prepareStatement(SQL);
             preparedStatement.setString(1, brand);
@@ -101,11 +104,17 @@ public class EditFoundLuggageController implements Initializable {
             preparedStatement.setString(11, material);
             preparedStatement.setDate(12, java.sql.Date.valueOf(date));
             preparedStatement.setString(13, flightNumber);
-            preparedStatement.setInt(14, 1);
+            preparedStatement.setInt(14, Main.employee.getEmployeeID());
+            preparedStatement.setInt(15, id);
             preparedStatement.executeUpdate();
         
             //Close connection
             conn.close();
+            
+            //Redirect to detail page of this luggage
+            AllLuggageController AllLuggage = new AllLuggageController();
+            AllLuggage.buildscreen(String.valueOf(id), "foundLuggage");
+            
         }catch(SQLException ex){
               Logger.getLogger(AddFoundLuggageController.class.getName()).log(Level.SEVERE,null,ex);
           }
@@ -118,12 +127,23 @@ public class EditFoundLuggageController implements Initializable {
     */
     public void buildScreen(String id) {
         try {
+            this.id = Integer.parseInt(id);
             Connection conn = Database.initDatabase();
-            String selectLuggage = "SELECT brand,color,type,weight,size,extra,material,flightNumber FROM luggage WHERE id = "+id;
+            String selectLuggage = "SELECT date,foundAirport,lostAirport,barcode,brand,color,type,weight,size,extra,material,flightNumber FROM luggage WHERE id = "+id;
             ResultSet rs = conn.createStatement().executeQuery(selectLuggage);
             if(rs.next()) {
                 txtExtra.setText(new String(rs.getBytes("extra"), "UTF-8"));
                 txtFlightNumber.setText(new String(rs.getBytes("flightNumber"), "UTF-8"));
+                cbBrand.setValue(new String(rs.getBytes("brand"), "UTF-8"));
+                cbColor.setValue(new String(rs.getBytes("color"), "UTF-8"));
+                cbType.setValue(new String(rs.getBytes("type"), "UTF-8"));
+                cbWeight.setValue(new String(rs.getBytes("weight"), "UTF-8"));
+                cbSize.setValue(new String(rs.getBytes("size"), "UTF-8"));
+                cbMaterial.setValue(new String(rs.getBytes("material"), "UTF-8"));
+                txtBarcode.setText(new String(rs.getBytes("barcode"), "UTF-8"));
+                txtDate.setText(new String(rs.getBytes("date"), "UTF-8"));
+                txtFoundAtAirport.setText(new String(rs.getBytes("foundAirport"), "UTF-8"));
+                txtLostAtAirport.setText(new String(rs.getBytes("lostAirport"), "UTF-8"));
             }
         } catch (SQLException | UnsupportedEncodingException ex) {
             Logger.getLogger(EditLostLuggageController.class.getName()).log(Level.SEVERE, null, ex);
