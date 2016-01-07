4d4a56a0075768545bcb4c4ddbce16297a84444a
Bas Broerse <basbroerse@hotmail.com>
2016-01-07 16:16:01 +0100

    password encryption

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -3,6 +3,8 @@ package controllers;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,71 +29,83 @@ import javafx.stage.Stage;
  * @author Mark
  */
 public class LoginController implements Initializable {
+
+    Encryption encryption = new Encryption();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    
-    @FXML private Label lblError;
+
+    @FXML
+    private Label lblError;
     /*
-    * This method validates the username and password 
-    * @param String username    Username from the employee
-    * @param String password    Password from the employee
-    * @return Boolean           If the username and password match a record. Return true
-    */
-    private Boolean loginValidation(String username, String password) {
+     * This method validates the username and password 
+     * @param String username    Username from the employee
+     * @param String password    Password from the employee
+     * @return Boolean           If the username and password match a record. Return true
+     */
+
+    private Boolean loginValidation(String username, String password) throws NoSuchAlgorithmException, InvalidKeySpecException {
         Boolean valid = false;
-        
-        try(Connection conn = Database.initDatabase()) {
+
+        try (Connection conn = Database.initDatabase()) {
             //Select the employee with the given username and password
-            String selectEmployee = 
-                    "SELECT id,userName,firstName,lastName,password,email,admin " +
-                    "FROM employee " +
-                    "WHERE userName = ? AND password = ?";
-            
+            String selectEmployee
+                    = "SELECT id,userName,firstName,lastName,"
+                    + "password,email,admin,salt "
+                    + "FROM employee "
+                    + "WHERE userName = ?";
+
             //Create prepared statment
             PreparedStatement preparedStatement = conn.prepareStatement(selectEmployee);
-            
+
             //set values
             preparedStatement.setString(1, username);
-            preparedStatement.setString(2, password);
-            
+
             //execute query and get results
             ResultSet employee = preparedStatement.executeQuery();
-       
+
             //if there are no records found.
-            if (!employee.next()) { 
+            if (!employee.next()) {
                 lblError.setText("Username and/or password is wrong");
                 lblError.setVisible(true);
             } else {
-                Main.employee.setUsername(employee.getString("firstName"));
-                Main.employee.setFunctionID(employee.getInt("admin"));
-                Main.employee.setEmployeeID(employee.getInt("id"));
-                valid = true;
+                byte[] salt = employee.getBytes("salt");
+                byte[] password2 = employee.getBytes("password");
+                Boolean auth = encryption.authenticate(password, password2, salt);
+                if (auth) {
+                    Main.employee.setUsername(employee.getString("firstName"));
+                    Main.employee.setFunctionID(employee.getInt("admin"));
+                    Main.employee.setEmployeeID(employee.getInt("id"));
+                    valid = true;
+                }
             }
             conn.close();
         } catch (SQLException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return valid;
     }
-    
-    @FXML private TextField txtUsername;
-    @FXML private TextField txtPassword;
-    @FXML private Button cbManager;
-    
+
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Button cbManager;
+
     /*
-    * This method is called if the employee clicked on the login button
-    * @param ActionEvent event
-    */
-    @FXML 
-    protected void Login(ActionEvent event) throws IOException {
+     * This method is called if the employee clicked on the login button
+     * @param ActionEvent event
+     */
+    @FXML
+    protected void Login(ActionEvent event) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         String username = txtUsername.getText();
         String password = txtPassword.getText();
-        
-        Boolean valid = loginValidation(username,password);
-        if(valid) {
+        Boolean valid = loginValidation(username, password);
+        if (valid) {
             //Get the main fxml
             Main main = new Main();
             
@@ -106,18 +120,18 @@ public class LoginController implements Initializable {
             
             //Add the fxml to the scene
             Scene scene = new Scene(root);
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            
             stage.hide();
             stage.setScene(scene);
             stage.show();
         }
     }
-    
+
     /*
-    * This method will load the login fxml into a borderpane
-    * @return borderpane    Returns the login view as borderpane
-    */
+     * This method will load the login fxml into a borderpane
+     * @return borderpane    Returns the login view as borderpane
+     */
     public BorderPane getLoginScreen() {
         BorderPane screen = null;
         try {
@@ -125,14 +139,14 @@ public class LoginController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return screen;
     }
-    
+
     /*
-    * If the decline button is pressed the application will close
-    *@param ActionEvent event
-    */
+     * If the decline button is pressed the application will close
+     *@param ActionEvent event
+     */
     @FXML
     protected void Logout(ActionEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
